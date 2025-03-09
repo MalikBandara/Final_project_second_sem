09e66fef568263d7399510274e5707ce9ad7f4fd
Malik <mmalith520@gmail.com>
2025-03-09 18:09:57 +0530

    project created

---
+++ src/main/java/org/example/secondsemlastp/SecondSemLastPApplication.java
@@ -0,0 +1,13 @@
+package org.example.secondsemlastp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSemLastPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondSemLastPApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/secondsemlastp/SecondSemLastPApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.secondsemlastp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSemLastPApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
