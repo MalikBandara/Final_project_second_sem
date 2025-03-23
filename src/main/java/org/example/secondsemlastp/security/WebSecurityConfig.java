890f976fd6b37c915f086ba665522c4cbe4b3f4f
Malik <mmalith520@gmail.com>
2025-03-23 12:24:37 +0530

    add spring security dependency

---
+++ src/main/java/org/example/secondsemlastp/security/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package org.example.secondsemlastp.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+
+
+}
